60ab26f4c401a951d4ef6d5d259a7b7adca9405e
Robert Plummer <robertleeplummerjr@gmail.com>
2019-01-07 08:53:55 -0500

    change tar path to original, simplify make-package.sh

--- darwin/make-package.sh
+++ darwin/make-package.sh
@@ -2,35 +2,27 @@
 
 echo 'writing release file angle-debug-darwin-x64.tar.gz'
 
-rm -rf Release
+# make tar
 rm angle-debug-darwin-x64.tar.gz
-
-mkdir Release
-
-cd Release
-cp ../angle/out/Debug/libangle_deqp_libegl.dylib .
-cp ../angle/out/Debug/libangle_deqp_libegl.dylib.TOC .
-cp ../angle/out/Debug/libangle_deqp_libgles2.dylib .
-cp ../angle/out/Debug/libangle_deqp_libgles2.dylib.TOC .
-cp ../angle/out/Debug/libangle_deqp_libgles3.dylib .
-cp ../angle/out/Debug/libangle_deqp_libgles3.dylib.TOC .
-cp ../angle/out/Debug/libangle_deqp_libgles31.dylib .
-cp ../angle/out/Debug/libangle_deqp_libgles31.dylib.TOC .
-cp ../angle/out/Debug/libangle_util.dylib .
-cp ../angle/out/Debug/libangle_util.dylib.TOC .
-cp ../angle/out/Debug/libEGL.dylib .
-cp ../angle/out/Debug/libEGL.dylib.TOC .
-cp ../angle/out/Debug/libfeature_support.dylib .
-cp ../angle/out/Debug/libfeature_support.dylib.TOC .
-cp ../angle/out/Debug/libGLESv1_CM.dylib .
-cp ../angle/out/Debug/libGLESv1_CM.dylib.TOC .
-cp ../angle/out/Debug/libGLESv2.dylib .
-cp ../angle/out/Debug/libGLESv2.dylib.TOC .
-
-cd ../
-
 tar -zcvf angle-debug-darwin-x64.tar.gz \
 angle/include \
-Release
+angle/out/Debug/libangle_deqp_libegl.dylib \
+angle/out/Debug/libangle_deqp_libegl.dylib.TOC \
+angle/out/Debug/libangle_deqp_libgles2.dylib \
+angle/out/Debug/libangle_deqp_libgles2.dylib.TOC \
+angle/out/Debug/libangle_deqp_libgles3.dylib \
+angle/out/Debug/libangle_deqp_libgles3.dylib.TOC \
+angle/out/Debug/libangle_deqp_libgles31.dylib \
+angle/out/Debug/libangle_deqp_libgles31.dylib.TOC \
+angle/out/Debug/libangle_util.dylib \
+angle/out/Debug/libangle_util.dylib.TOC \
+angle/out/Debug/libEGL.dylib \
+angle/out/Debug/libEGL.dylib.TOC \
+angle/out/Debug/libfeature_support.dylib \
+angle/out/Debug/libfeature_support.dylib.TOC \
+angle/out/Debug/libGLESv1_CM.dylib \
+angle/out/Debug/libGLESv1_CM.dylib.TOC \
+angle/out/Debug/libGLESv2.dylib \
+angle/out/Debug/libGLESv2.dylib.TOC
 
 echo 'wrote release file angle-debug-darwin-x64.tar.gz'
